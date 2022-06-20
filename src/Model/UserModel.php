70dd4fef1d05986a1c4ffb3693e95a9d31b55491
Dylan Germann <dev.dyger@gmail.com>
2022-06-20 14:20:49 +0200

    feat: convert the collection of entity to an array of model for UserModel class atm

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -2,6 +2,8 @@
 
 namespace App\Model;
 
+use App\Entity\Message;
+use App\Entity\Trick;
 use App\Entity\User;
 use Doctrine\Common\Collections\Collection;
 
@@ -10,22 +12,44 @@ class UserModel
     public int $id;
     public string $email;
     public string $username;
+    public array $tricks;
+    public array $messages;
     private array $roles = [];
     private string $password;
-    public Collection $tricks;
-    public Collection $messages;
 
     /**
      * @param User $userEntity
      */
     public function __construct(User $userEntity)
     {
-        $this->id = $userEntity->getId();
-        $this->email = $userEntity->getEmail();
+        $this->id       = $userEntity->getId();
+        $this->email    = $userEntity->getEmail();
         $this->username = $userEntity->getUsername();
-        $this->roles = $userEntity->getRoles();
+        $this->roles    = $userEntity->getRoles();
         $this->password = $userEntity->getPassword();
-        $this->tricks = $userEntity->getTricks();
-        $this->messages = $userEntity->getMessages();
+        $this->initTricksArray($userEntity->getTricks());
+        $this->initMessagesArray($userEntity->getMessages());
+    }
+
+    /**
+     * @param Collection $messagesCollection
+     */
+    private function initMessagesArray(Collection $messagesCollection): void
+    {
+        $this->messages = [];
+        foreach ($messagesCollection as $messageEntity) {
+            $this->messages[] = new MessageModel($messageEntity);
+        }
+    }
+
+    /**
+     * @param Collection $tricksCollection
+     */
+    private function initTricksArray(Collection $tricksCollection): void
+    {
+        $this->tricks = [];
+        foreach ($tricksCollection as $trickEntity) {
+            $this->tricks[] = new TrickModel($trickEntity);
+        }
     }
 }
