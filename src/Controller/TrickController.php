6ff230b488f54925a97ff62561daf7042e813318
Dylan Germann <dev.dyger@gmail.com>
2022-06-24 12:07:16 +0200

    feat: add a conditions for check if the trick exist and return a model

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
+use App\Service\TrickService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class TrickController extends AbstractController
 {
     #[Route('/trick/{id}', name: 'trick_detail')]
-    public function showTrick(int $id): Response
+    public function showTrick(int $id, TrickRepository $trickRepository): Response
     {
+        TrickService::getTrickById($id, $trickRepository);
         return $this->render('trick/index.html.twig', [
             'controller_name' => 'TrickController',
         ]);
--- src/Service/TrickService.php
+++ src/Service/TrickService.php
@@ -3,13 +3,26 @@
 namespace App\Service;
 
 use App\Model\TrickModel;
+use App\Repository\TrickRepository;
+use Exception;
 use JetBrains\PhpStorm\Pure;
+use Symfony\Component\HttpFoundation\Response;
 
 class TrickService
 {
 
     public function __construct()
     {
+    }
+
+    public static function getTrickById(int $id, TrickRepository $trickRepository)
+    {
+
+        $trickEntity = $trickRepository->find($id);
+        if (empty($trickEntity)) {
+            throw new Exception("Le trick ayant pour id $id n'existe pas !!", Response::HTTP_NO_CONTENT);
+        }
+        return new TrickModel($trickEntity);
     }
 
     /**
