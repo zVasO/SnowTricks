e6a0c8a2268c24b3c1b0babb7c3468191f32487a
Dylan Germann <dev.dyger@gmail.com>
2022-06-24 11:56:56 +0200

    feat: added link to the trick detail in home page

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
 {
-    #[Route('/trick/{id}', name: 'app_trick')]
+    #[Route('/trick/{id}', name: 'trick_detail')]
     public function showTrick(int $id): Response
     {
         return $this->render('trick/index.html.twig', [
