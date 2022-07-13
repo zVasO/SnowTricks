ab3452872e583cf1ba18bb51faf8241035b450e2
Dylan Germann <dev.dyger@gmail.com>
2022-07-13 14:51:05 +0200

    feat: error template

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,33 +4,25 @@ namespace App\EventListener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Twig\Environment;
 
 class ExceptionListener
 {
+
+
+    public function __construct(private Environment $environment)
+    {
+    }
+
     public function onKernelException(ExceptionEvent $event)
     {
         // You get the exception object from the received event
         $exception = $event->getThrowable();
-        $message = sprintf(
-            'Oups: %s Code d\'erreur: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
-
-        // Customize your response object to display the exception details
         $response = new Response();
-        $response->setContent($message);
-
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-
-        // sends the modified response object to the event
+        $response->setContent($this->environment->render('error.html.twig', [
+            'error_code' => $exception->getCode(),
+            'error_message' => $exception->getMessage(),
+        ]));
         $event->setResponse($response);
     }
 }
